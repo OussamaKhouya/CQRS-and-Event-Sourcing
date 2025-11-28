5cc6eebaa7e690f359c810ad6ea4b83d37001610
Oussama Khouya <ouskhouya@gmail.com>
2025-11-28 16:51:00 +0100

    from H2 to PostgreSQL and add eventStore

--- src/main/java/ma/enset/cqrseventsourcing/command/controllers/AccountCommandController.java
+++ src/main/java/ma/enset/cqrseventsourcing/command/controllers/AccountCommandController.java
@@ -4,19 +4,23 @@ import lombok.RequiredArgsConstructor;
 import ma.enset.cqrseventsourcing.command.commands.AddAccountCommand;
 import ma.enset.cqrseventsourcing.command.dtos.AddNewAccountRequestDTO;
 import org.axonframework.commandhandling.gateway.CommandGateway;
+import org.axonframework.eventsourcing.eventstore.EventStore;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.UUID;
 import java.util.concurrent.CompletableFuture;
+import java.util.stream.Stream;
 
 @RestController
 @RequestMapping("/commands/accounts")
 public class AccountCommandController {
 
     private CommandGateway commandGateway;
+    private EventStore eventStore;
 
-    public AccountCommandController(CommandGateway commandGateway) {
+    public AccountCommandController(CommandGateway commandGateway, EventStore eventStore) {
         this.commandGateway = commandGateway;
+        this.eventStore = eventStore;
     }
 
     @PostMapping("/add")
@@ -32,5 +36,10 @@ public class AccountCommandController {
     @ExceptionHandler(Exception.class)
     public String exceptionHandler(Exception  exception){
         return exception.getMessage();
+    }
+
+    @GetMapping("/events/{accountId}")
+    public Stream eventStore(@PathVariable String accountId){
+        return eventStore.readEvents(accountId).asStream();
     }
 }
